26664447f83840b6f8d4b8cf4eba44cadbad4a85
INADA Naoki <inada-n@klab.com>
2013-08-30 20:43:26 +0900

    Add sleep example.

---
+++ examples/sleep.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+from __future__ import print_function
+
+import tornado.ioloop
+import greenlet
+from functools import wraps
+import mykaze
+
+
+def within_greenlet(func):
+    @wraps(func)
+    def meth(*args, **kw):
+        g = greenlet.greenlet(lambda: func(*args, **kw))
+        return g.switch()
+    return meth
+
+@within_greenlet
+def sample(n):
+    con = mykaze.connect('localhost', 'root', 'root')
+    print('connected')
+    cur = con.cursor()
+    for i in range(100):
+        cur.execute("SELECT SLEEP(1)")
+        print(n, i, cur.fetchone()[0])
+
+io_loop = tornado.ioloop.IOLoop.instance()
+for n in range(20):
+    io_loop.add_callback(sample, n)
+io_loop.start()
